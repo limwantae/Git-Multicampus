2b9649f352de59598176fd70311288f6c3368b18
limwantae <apts2@naver.com>
2023-05-30 14:33:17 +0900

    sql수정

--- src/main/java/com/main/SQLInjectionExample.java
+++ src/main/java/com/main/SQLInjectionExample.java
@@ -1,13 +1,15 @@
 package com.main;
 
 public class SQLInjectionExample {
-    public static void main(String[] args) {
-        String username = getUserInput();
-        String query = "SELECT * FROM users WHERE username = '"+username+"'";
-        System.out.println(query);
-    }
+        public static void main(String[] args) {
+            String username = getUserInput();
+            String query = "SELECT * FROM users WHERE username = '" + username + "'";
+            // query 실행 로직
+        }
 
-    private static String getUserInput (){
-        return "admin' --";
+        private static String getUserInput() {
+            // 사용자로부터 입력 받는 로직
+            return "TEST";
+        }
     }
 }
